2fc31ba51439cc4d7d7c1a271a06f9609aacc9bc
Mathieu DE BAERDEMAEKER <m.de-baerdemaeker@etudiant.univ-lille1.fr>
2015-09-22 11:00:34 +0200

    Test pull

---
+++ src/GUI/FenetreHappy.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class FenetreHappy extends JFrame{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public FenetreHappy(String nom) {
+    this.setTitle(nom);
+  }
+
+}
