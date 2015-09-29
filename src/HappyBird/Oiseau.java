a5fc36c7da7b05becbff3af53f690c0959a61b99
lopezb <benjamin-lopez@hotmail.fr>
2015-09-29 15:12:16 +0200

    Modification fu

--- src/HappyBird/Oiseau.java
+++ src/HappyBird/Oiseau.java
@@ -2,10 +2,6 @@ package HappyBird;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Oiseau extends JPanel {
@@ -19,11 +15,11 @@ public class Oiseau extends JPanel {
 	//Vitesse de deplacement de l
 	private int speed = 1;
 
-	//La couleur de la balle
-	private Color c = Color.BLACK;
-        
-        private int x;
-        private int y;
+	//Les couleurs de l'oiseau.
+                
+        //Constantes.
+        public static final Color BIRD_BODY_COLOR = Color.RED;
+        public static final Color BIRD_BEAK_COLOR = Color.BLACK;
 	
 	/* Constructeur */
 
@@ -55,11 +51,13 @@ public class Oiseau extends JPanel {
 	 * @param g
 	 *            le moteur graphique
 	 */
-	public void paintComponent(Graphics g) {
-		g.setColor(Color.RED);
-		//g2d.fillRect(posX, posY, 8, 8);
-		g.fillOval(this.x, this.y, 25, 25);
-		g.setColor(Color.BLACK);
-		g.fillArc(this.x+20,this.y-5,20,35,170,20);  /*Fonction du fillArc (position X, position Y, */
-	}
+//	public void paintComponent(Graphics g) {
+//		g.setColor(Color.RED);
+//		//g2d.fillRect(posX, posY, 8, 8);
+//		g.fillOval(position.getX(), position.getY(), 25, 25);
+//		g.setColor(Color.BLACK);
+//		g.fillArc(this.x+20,this.y-5,20,35,170,20);  /*Fonction du fillArc (position X, position Y, */
+//	}
+        
+        
 }
