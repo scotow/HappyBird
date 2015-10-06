c44d04783af8533f3a3099512ddbe4b50136fc94
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-10-06 14:20:07 +0200

    Update conflic

--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -32,8 +32,6 @@ public class GamePanel extends JPanel{
     private Mathematique courbeBezier;
     private List<Coordonnee> listPoint;
     private List<Coordonnee> listPoint2;
-    private int width;
-    private int height;
     private int buffer;
     private Random rd = new Random();
     
@@ -43,62 +41,64 @@ public class GamePanel extends JPanel{
      * @param x : position x du panel
      * @param y : position y du panel
      */
-    public GamePanel(int width, int height) {
-        this.oiseau = new Oiseau();
+    public GamePanel() {
         this.listeObs = new ArrayList <Obstacle>();
         this.listPoint = new ArrayList<Coordonnee>();
         this.listPoint2 = new ArrayList<Coordonnee>();
-        this.width = width;
-        this.height = height;
-        listPoint.add(new Coordonnee(1, 0)); // Premier point
+        //test.setListPoint(new Coordonnee(600, 250));
+        //this.listeObs.add(new TrucBleu(100,200,0));
+        this.b = new Obstacle (100,200,50);
+        this.add(b);
+        this.initCompenent();
+        //Ajoute un par un les TrucBleu 
+        /*for (int i = 0 ; i < this.listeObs.size() ; i++) {
+        	this.add(this.listeObs.get(i));
+        }*/
+        setBackground(Color.BLACK);
+    }
+    
+    public void initCompenent(){
+        this.oiseau = new Oiseau(600);
+        System.out.println(this.getHeight());
+        listPoint.add(new Coordonnee(this.oiseau.getPosition().getX()+(Oiseau.BIRD_BODY_RADIUS/2), this.oiseau.getPosition().getY())); // Premier point
         listPoint.add(new Coordonnee(rd.nextInt(20)+50, rd.nextInt(20)+120)); // Deuxieme point
-        listPoint.add(new Coordonnee(rd.nextInt(50)+250, rd.nextInt(50)+200)); // Troisieme point
+        listPoint.add(new Coordonnee(rd.nextInt(50)+550, rd.nextInt(50)+200)); // Troisieme point
         //listPoint.add(new Coordonnee(600, rd.nextInt(80)+250));
         listPoint2.add(new Coordonnee(1, 0)); // Premier point
         listPoint2.add(new Coordonnee(rd.nextInt(20)+50, rd.nextInt(20)+120)); // Deuxieme point
         listPoint2.add(new Coordonnee(rd.nextInt(50)+250, rd.nextInt(50)+200)); // Troisieme point
         //listPoint.add(new Coordonnee(600, rd.nextInt(80)+250));
         courbeBezier = new Mathematique(listPoint, listPoint2);
-        //test.setListPoint(new Coordonnee(600, 250));
-        //this.listeObs.add(new TrucBleu(100,200,0));
-        //Ajoute un par un les TrucBleu 
-        /*for (int i = 0 ; i < this.listeObs.size() ; i++) {
-        	this.add(this.listeObs.get(i));
-        }*/
-        setBackground(Color.BLACK);
-        Obstacle o;
-        Random r = new Random();
-        int x, y;
+          Random r = new Random();
         for (int i = 0 ; i < 10 ; i++) {
-        	o = new Obstacle(r.nextInt(500), r.nextInt(350), 15);
-        	listeObs.add(o);
+        	b = new Obstacle(r.nextInt(200)+380, r.nextInt(350), 15);
+        	listeObs.add(b);
         }
     }
 
     @Override
     public void paintComponent(Graphics g) {
        // super.paintComponents(g); //To change body of generated methods, choose Tools | Templates.
+        try {
+            for (double i = 0; i < 1; i+=0.02) {
+		g.setColor(Color.BLUE);
+		g.fillOval((int)courbeBezier.calculerPoint(i).getX(),(int)courbeBezier.calculerPoint(i).getY(), 6, 6);
+		g.setColor(Color.ORANGE);
+		g.fillOval((int)courbeBezier.calculerPoint2(i).getX(),(int)courbeBezier.calculerPoint2(i).getY(), 6, 6);
+            }
+        } catch (PointCourbeException e) {
+            e.getMessage();
+        }
         g.setColor(Oiseau.BIRD_BODY_COLOR);
         g.fillOval((int)oiseau.getPosition().getX(), (int)oiseau.getPosition().getY(), Oiseau.BIRD_BODY_RADIUS, Oiseau.BIRD_BODY_RADIUS);
-        try {
-			for (double i = 0.0; i < 1; i+=0.01) {
-				g.setColor(Color.BLUE);
-				g.fillOval((int)courbeBezier.calculerPoint(i).getX(),(this.height-30)-(int)courbeBezier.calculerPoint(i).getY(), 3, 3);
-				g.setColor(Color.ORANGE);
-				g.fillOval((int)courbeBezier.calculerPoint2(i).getX(),(this.height-30)-(int)courbeBezier.calculerPoint2(i).getY(), 3, 3);
-			}
-		} catch (PointCourbeException e) {
-			e.getMessage();
-		}
-        
         g.setColor(Color.blue);
         for (int i = 0 ; i < this.listeObs.size(); i++) {
         	g.fillOval(listeObs.get(i).getX(), listeObs.get(i).getY(), 15,15);
         }
-        
     }
     
     
     
     
 }
+
