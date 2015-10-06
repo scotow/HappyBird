042eb75f39264a42eb8f6bedeb33c5dc509bae93
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-10-06 20:59:38 +0200

    Correction des courbes de beziers

--- src/HappyBird/Courbe.java
+++ src/HappyBird/Courbe.java
@@ -38,88 +38,37 @@ public class Courbe {
         Random rand = new Random();
         listPoint.add(new Coordonnee(beginning.getX()+(Oiseau.BIRD_BODY_RADIUS/2), beginning.getY())); // Premier point
         listPoint.add(new Coordonnee(rand.nextInt(20)+50, rand.nextInt(20)+120)); // Deuxieme point
-        listPoint.add(new Coordonnee(rand.nextInt(50)+550, rand.nextInt(50)+200)); // Troisieme point
+        listPoint.add(new Coordonnee(rand.nextInt(50)+500, rand.nextInt(50)+100)); // Troisieme point
+        listPoint.add(new Coordonnee(rand.nextInt(50)+550, rand.nextInt(50)+530)); // Quatrieme point
     }
 
   public Coordonnee calculerPoint(double t) throws PointCourbeException {
-	double coordX = 0.0;
-	double coordY = 0.0;
-    if (listPoint.size() <= 3) {
-    	/*Coordonnee coord = calculeBezier(listPoint, 1, 1, t);
-        coordX = coord.getX();
-        coordY = coord.getY();*/
-      switch (listPoint.size()) {
-        case 1:
-          coordX = listPoint.get(0).getX();
-          coordY = listPoint.get(0).getY();
-          break;
-        case 2:
-          coordX = formulBezier(listPoint.get(0).getX(), listPoint.get(1).getX(), t);
-          coordY = formulBezier(listPoint.get(0).getY(), listPoint.get(1).getY(), t);
-          break;
-        case 3:
-          coordX =
-              formulBezier(formulBezier(listPoint.get(0).getX(), listPoint.get(1).getX(), t),
-                  formulBezier(listPoint.get(1).getX(), listPoint.get(2).getX(), t), t);
-          coordY =
-              formulBezier(formulBezier(listPoint.get(0).getY(), listPoint.get(1).getY(), t),
-                  formulBezier(listPoint.get(1).getY(), listPoint.get(2).getY(), t), t);
-          
-          break;
-        /*case 4:
-          coordX =
-              formulBezier(
-                  formulBezier(formulBezier(listPoint.get(0).getX(), listPoint.get(1).getX(), t),
-                      formulBezier(listPoint.get(1).getX(), listPoint.get(2).getX(), t), t),
-                  formulBezier(formulBezier(listPoint.get(1).getX(), listPoint.get(2).getX(), t),
-                      formulBezier(listPoint.get(2).getX(), listPoint.get(3).getX(), t), t), t);
-          coordY =
-              formulBezier(
-                  formulBezier(formulBezier(listPoint.get(0).getY(), listPoint.get(1).getY(), t),
-                      formulBezier(listPoint.get(1).getY(), listPoint.get(2).getY(), t), t),
-                  formulBezier(formulBezier(listPoint.get(1).getY(), listPoint.get(2).getY(), t),
-                      formulBezier(listPoint.get(2).getY(), listPoint.get(3).getY(), t), t), t);
-          break;*/
-        default:
-          coordX = 0.0;
-          coordY = 0.0;
-          break;
-      }
-        //coordX = calculeBezier(t).getX();
-       // coordY = calculeBezier(t).getY();
-    } else {
-      throw new PointCourbeException("Trop de point !");
-    }
+    Coordonnee bezier = formulBezier(this.listPoint, t);
+	double coordX = bezier.getX();
+	double coordY = bezier.getY();
     return new Coordonnee(coordX, coordY);
   }
-  
-  /*public Coordonnee calculerPoint2(double t) throws PointCourbeException {
-		double coordX = 0.0;
-		double coordY = 0.0;
-	    if (listPoint2.size() <= 4) {
-	    	Coordonnee coord = calculeBezier(listPoint2,listPoint2.get(0).getX(),listPoint2.get(0).getY(),t);
-	        coordX = coord.getX();
-	        coordY = coord.getY();
-	    } else {
-	      throw new PointCourbeException("Trop de point !");
-	    }
-	    return new Coordonnee(coordX, coordY);
-	  }
-  */
-  /*public Coordonnee calculeBezier(List<Coordonnee> listPoint, double t) {
-    if (listPoint.size()-1 == 0) {
-        return new Coordonnee(listPoint.get(0).getX(), listPoint.get(0).getY());
-    }
-    for (int i = 0; i < listPoint.size(); i++) {
-       x = formulBezier(listPoint.get(i).getX(), listPoint.get(i+1).getX(), t);
-       y = formulBezier(listPoint.get(i).getY(), listPoint.get(i+1).getY(), t);
-    }
-    
-    return calculeBezier(listPoint, t);
-  }*/
 
   public double formulBezier(double point1, double point2, double t) {
     return ((1 - t) * point1) + (t * point2);
+  }
+  
+  public Coordonnee formulBezier(List<Coordonnee> listpoint, double t){
+    if (listpoint.size() > 4 || listpoint.isEmpty()) {
+      return new Coordonnee(0, 0);
+    }
+    return new Coordonnee(formuleBezierParam((listpoint.get(0) == null)? 0.0:listpoint.get(0).getX(), // Pour les X
+                                             (listpoint.get(1) == null)? 0.0:listpoint.get(1).getX(),
+                                             (listpoint.get(2) == null)? 0.0:listpoint.get(2).getX(),
+                                             (listpoint.get(3) == null)? 0.0:listpoint.get(3).getX(),t),
+                         (formuleBezierParam((listpoint.get(0) == null)? 0.0:listpoint.get(0).getY(), // Pour les Y
+                                             (listpoint.get(1) == null)? 0.0:listpoint.get(1).getY(),
+                                             (listpoint.get(2) == null)? 0.0:listpoint.get(2).getY(),
+                                             (listpoint.get(3) == null)? 0.0:listpoint.get(3).getY(),t)));
+  }
+  
+  public double formuleBezierParam(double p0, double p1, double p2, double p3, double t){
+    return (p0*Math.pow((1-t), 3)) + (((3*p1)*t)*Math.pow((1-t), 2)) + (((3*p2)*Math.pow(t, 2))*(1-t)) + p3*Math.pow(t, 3);
   }
 
   public List<Coordonnee> getListPoint() {
@@ -137,9 +86,9 @@ public class Courbe {
 	  return this.listPoint.isEmpty();
   }
   
-  public double tangente(double x, double y){
-      double a = (x/y);
-      double b = y-(a*x);
-      return (a*x)+b;
+  public double tangente(double t){
+      
+    
+      return t;
   }
 }
