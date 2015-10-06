4178246bd33a7ab00e7bc94eca627db166106a4f
Yann Garbé <yanngarbe@gmail.com>
2015-10-06 15:19:06 +0200

    Ajout de deux test de collision pour la suite

--- src/Test/TestDeplacement.java
+++ src/Test/TestDeplacement.java
@@ -4,13 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import HappyBird.Coordonnee;
+import HappyBird.Obstacle;
+
 public class TestDeplacement {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-        
-        
 
+	
+    @Test 
+	public void testAjoutCoordonneesObstacle() {
+		Obstacle o = new Obstacle (50,50,15);
+		o.getCoord().ajout(2.00, 2.00);
+		Coordonnee c = new Coordonnee(52,52);
+		assertTrue(c.equals(o.getCoord()));
+	}
+    
+    @Test
+    public void testCollisionBordureObstacleVrai() {
+    	Obstacle o1 = new Obstacle (50,50,15);
+    	Obstacle o2 = new Obstacle (55,55,15);
+    	assertTrue(o1.collision(o2));
+    }
+
+    @Test
+    public void testCollisionBordureObstacleFaux() {
+    	Obstacle o1 = new Obstacle(50,50,15);
+    	Obstacle o2 = new Obstacle(80,80,15);
+    	assertFalse(o1.collision(o2));
+    }
 }
