413b6869cd22842154868966fef679ee63d968eb
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-10-09 22:39:46 +0200

    Envoie de la classe Bounds.

--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -22,54 +22,60 @@ import javax.swing.*;
  * 
  * @author debaerdm
  */
-public class GamePanel extends JPanel implements KeyListener{
-    
-    private Plateau plateau;
+public class GamePanel extends JPanel implements KeyListener {
 
-    public GamePanel() {
-        setFocusable(true);
-        addKeyListener(this);
+	private static final long serialVersionUID = 1L;
+	private Plateau plateau;
 
-        plateau = new Plateau(this);
-        //setBackground(Color.BLACK);
-        repaint();
-    }
+	public GamePanel() {
+		setFocusable(true);
+		addKeyListener(this);
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
-        g.drawImage(new ImageIcon(getClass().getResource("/GUI/images/background.png")).getImage(), 0, 0, null);
-        try {
-            g.setColor(Color.BLUE);
-            for (double i = 0; i < plateau.getOiseau().getTemps() ; i+= plateau.getOiseau().getSpeed()) {
-                g.fillOval((int)plateau.getOiseau().getCourbe().calculerPoint(i).getX(),(int)plateau.getOiseau().getCourbe().calculerPoint(i).getY(), 6, 6);
-            }
-        } catch (PointCourbeException e) {
-            e.getMessage();
-        }
-		g.setColor((plateau.getOiseau().detectionCollision())? Oiseau.BIRD_BEAK_COLOR : Oiseau.BIRD_BODY_COLOR);
-        g.fillOval((int)plateau.getOiseau().getPosition().getX()-Oiseau.BIRD_BODY_RADIUS/2, (int)plateau.getOiseau().getPosition().getY()-Oiseau.BIRD_BODY_RADIUS/2, Oiseau.BIRD_BODY_RADIUS, Oiseau.BIRD_BODY_RADIUS);
-        g.setColor(Color.ORANGE);
-        for (int i = 0 ; i < plateau.getObstacles().size(); i++) {
-        	g.fillOval((int)plateau.getObstacles().get(i).getX(), (int)plateau.getObstacles().get(i).getY(), Obstacle.RADIUS, Obstacle.RADIUS);
-        }
-    }
+		plateau = new Plateau(this);
+		// setBackground(Color.BLACK);
+		repaint();
+	}
 
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g); // To change body of generated methods, choose
+									// Tools | Templates.
+		g.drawImage(new ImageIcon(getClass().getResource("/GUI/images/background.png")).getImage(), 0, 0, null);
+		try {
+			g.setColor(Color.BLUE);
+			for (double i = 0; i < plateau.getOiseau().getTemps(); i += plateau.getOiseau().getSpeed()) {
+				g.fillOval((int) plateau.getOiseau().getCourbe().calculerPoint(i).getX(),
+						(int) plateau.getOiseau().getCourbe().calculerPoint(i).getY(), 6, 6);
+			}
+		} catch (PointCourbeException e) {
+			e.getMessage();
+		}
+		g.setColor((plateau.getOiseau().detectionCollision()) ? Oiseau.BIRD_BEAK_COLOR : Oiseau.BIRD_BODY_COLOR);
+		g.fillOval((int) plateau.getOiseau().getPosition().getX() - Oiseau.BIRD_BODY_RADIUS / 2,
+				(int) plateau.getOiseau().getPosition().getY() - Oiseau.BIRD_BODY_RADIUS / 2, Oiseau.BIRD_BODY_RADIUS,
+				Oiseau.BIRD_BODY_RADIUS);
+		g.setColor(Color.ORANGE);
+		for (int i = 0; i < plateau.getObstacles().size(); i++) {
+			g.fillOval((int) plateau.getObstacles().get(i).getX(), (int) plateau.getObstacles().get(i).getY(),
+					Obstacle.RADIUS, Obstacle.RADIUS);
+		}
+	}
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        if(e.getKeyChar() == ' ')
-            this.plateau = new Plateau(this);
-    }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if (e.getKeyChar() == ' ')
+			this.plateau = new Plateau(this);
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {}
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
 
-    @Override
-    public void keyReleased(KeyEvent e) {}
-    
-    public Plateau getPlateau() {
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public Plateau getPlateau() {
 		return plateau;
 	}
 }
-
