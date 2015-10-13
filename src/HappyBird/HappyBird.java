4ed4416bf4cc5a4a690cb5057d7c4dac39899a9b
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-10-14 00:08:44 +0200

    Suprresion des commentaires inutiles.

--- src/HappyBird/HappyBird.java
+++ src/HappyBird/HappyBird.java
@@ -9,13 +9,6 @@ public class HappyBird {
    * @param args
    */
   public static void main(String[] args) {
-//    javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//
-//      @Override
-//      public void run() {
-//         new MainFrame();
-//      }
-//    });
     new MainFrame();
   }
 
