dd3ec6ac979fbd4cc82d40cd6455469015e2fd0f
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-10-14 00:11:12 +0200

    Ajout de la classe Constante possédant toutes les constantes du programmes.

---
+++ src/HappyBird/Constante.java
@@ -0,0 +1,24 @@
+package HappyBird;
+
+
+import java.awt.Color;
+
+public class Constante {
+
+  public static final Color BIRD_BODY_COLOR = Color.RED;
+  public static final Color BIRD_BEAK_COLOR = Color.BLACK;
+
+  public static final int BIRD_BODY_RADIUS = 25;
+  
+  public static final double CONSTANTE_GRAVITATIONNELLE = 9.81;
+  public static final double PI = Math.PI;
+  
+  public static final double VITESSE_X = 2;
+  public static final double VITESSE_GRAVITAIONNELLE = 0.10;
+  public static final double VITESSE_LANCER = -4;
+  
+  public static final int OBSTACLE_RADIUS = 15;
+
+  public static final Color OBSTACLE_NOT_TOUCHED_COLOR = Color.ORANGE;
+  public static final Color OBSTACLE_TOUCHED_COLOR = Color.MAGENTA;
+}
