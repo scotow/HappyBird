44328a28da4ee4d3a5ad7f973c74a6a9abb5c6c4
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-11-01 11:10:17 +0100

    Mise en place de la pseudo formule pour la tangente.

--- src/HappyBird/Object/Courbe.java
+++ src/HappyBird/Object/Courbe.java
@@ -13,80 +13,131 @@ import java.util.List;
  */
 public class Courbe {
 
-  private List<Coordonnee> listePointTangente;
-  private double coordX;
-  private double coordY;
-  private double coordXDeriv;
-  private double coordYDeriv;
-  private int vitesseInitial = 10;
-  private int angleInitial = (int) (Constante.PI/3);
-  private double vitesseY = Constante.VITESSE_LANCER;
-  
-  //////////////////Calcule vitesses horizontale et verticale ///////////
-  /*private double vitesseX = Math.cos(angleInitial)*vitesseInitial;
-  private double vitesseY = Math.sin(angleInitial)*vitesseInitial;*/
+	private double coordX;
+	private double coordY;
+	private double coordXDeriv;
+	private double coordYDeriv;
+	private int vitesseInitial = 10;
+	private int angleInitial = (int) (Constante.PI / 3);
+	private double vitesseY = Constante.VITESSE_LANCER;
 
-  public Coordonnee calculerPoint(List<Coordonnee> listPoint, double t){
-    /*Coordonnee bezier = null;
-     bezier = formulBezier(listPoint, t);
-    listePointTangente.add(bezier);*/
-    return formulBezier(listPoint, t);
-  }
+	////////////////// Calcule vitesses horizontale et verticale ///////////
+	/*
+	 * private double vitesseX = Math.cos(angleInitial)*vitesseInitial; private
+	 * double vitesseY = Math.sin(angleInitial)*vitesseInitial;
+	 */
 
-  public Coordonnee formulBezier(List<Coordonnee> listpoint, double t){
-    return new Coordonnee(formuleBezierParam((listpoint.get(0) == null)? 0.0:listpoint.get(0).getX(), // Pour les X
-                                             (listpoint.get(1) == null)? 0.0:listpoint.get(1).getX(),
-                                             (listpoint.get(2) == null)? 0.0:listpoint.get(2).getX(),
-                                             (listpoint.get(3) == null)? 0.0:listpoint.get(3).getX(),t),
-                         (formuleBezierParam((listpoint.get(0) == null)? 0.0:listpoint.get(0).getY(), // Pour les Y
-                                             (listpoint.get(1) == null)? 0.0:listpoint.get(1).getY(),
-                                             (listpoint.get(2) == null)? 0.0:listpoint.get(2).getY(),
-                                             (listpoint.get(3) == null)? 0.0:listpoint.get(3).getY(),t)));
-  }
-  
-  public double formuleBezierParam(double p0, double p1, double p2, double p3, double t){
-    return (p0*Math.pow((1-t), 3)) + (((3*p1)*t)*Math.pow((1-t), 2)) + (((3*p2)*Math.pow(t, 2))*(1-t)) + p3*Math.pow(t, 3);
-  }
+	public Coordonnee calculerPoint(List<Coordonnee> listPoint, double t) {
+		return formulBezier(listPoint, t);
+	}
 
-  public double tangente(double point, double bezier, double bezierDeriv){
-      return (point-bezier)/bezierDeriv;
-  }
-  
-  
-  public double getCoordX() {
-    return coordX;
-  }
+	public Coordonnee calculerPointDerive(List<Coordonnee> listPoint, double t) {
+		return formulBezierDeriver(listPoint, t);
+	}
 
+	public Coordonnee calculerTangente(double x, double y, List<Coordonnee> coordonnees, double t) {
+		double xtmp = ((x - calculerPoint(coordonnees, t).getX()) / (calculerPointDerive(coordonnees, t).getX()));
+		double ytmp = ((y - calculerPoint(coordonnees, t).getY()) / (calculerPointDerive(coordonnees, t).getY()));
+		return new Coordonnee(xtmp, ytmp);
+	}
 
-  public double getCoordY() {
-    return coordY;
-  }
+	public Coordonnee formulBezier(List<Coordonnee> listpoint, double t) {
+		double x = 0;
+		double xtmp = 0;
+		double ytmp = 0;
+		double y = 0;
+		for (int i = listpoint.size(); i > 0; i--) {
+			x += formuleBezierTest(0, listpoint.get(i - 1).getX(), t, i);
+			xtmp = x;
+			y += formuleBezierTest(0, listpoint.get(i - 1).getY(), t, i);
+			ytmp = y;
+		}
+		return new Coordonnee(xtmp, ytmp);
+	}
 
+	public Coordonnee formulBezierDeriver(List<Coordonnee> point, double t) {
+		double x = 0;
+		double xtmp = 0;
+		double ytmp = 0;
+		double y = 0;
+		for (int i = point.size() - 1; i > 1; i--) {
+			x += formuleBezierDeriveTest(0, point.get(i - 1).getX(), point.get(i).getX(), t, i);
+			xtmp = x;
+			y += formuleBezierDeriveTest(0, point.get(i - 1).getY(), point.get(i).getY(), t, i);
+			ytmp = y;
+		}
+		return new Coordonnee(xtmp, ytmp);
+	}
 
-  public double getCoordXDeriv() {
-    return coordXDeriv;
-  }
+	// (p0*Math.pow((1-t), 3)) + (((3*p1)*t)*Math.pow((1-t), 2)) +
+	// (((3*p2)*Math.pow(t, 2))*(1-t)) + p3*Math.pow(t, 3);
+	public double formuleBezierTest(double resultat, double listPoint, double t, int size) {
+		switch (size) {
+		case 4:
+			resultat += listPoint * Math.pow(t, 3);
+			break;
+		case 3:
+			resultat += (((3 * listPoint) * Math.pow(t, 2)) * (1 - t));
+			break;
+		case 2:
+			resultat += (((3 * listPoint) * t) * Math.pow((1 - t), 2));
+			break;
+		case 1:
+			resultat += (listPoint * Math.pow((1 - t), 3));
+			break;
+		default:
+			break;
+		}
 
+		return resultat;
+	}
 
-  public double getCoordYDeriv() {
-    return coordYDeriv;
-  }
+	// 3*(((p1-p0)*Math.pow((1-t), 2)) + (2*(p2-p1)*t*(t-1)) +
+	// ((p3-p2)*Math.pow(t, 2)));
+	public double formuleBezierDeriveTest(double resultat, double listPoint1, double listePoint2, double t, int size) {
+		switch (size) {
+		case 3:
+			resultat += ((listePoint2 - listPoint1) * Math.pow(t, 2));
+			break;
+		case 2:
+			resultat += (2 * (listePoint2 - listPoint1) * t * (t - 1));
+			break;
+		case 1:
+			resultat += ((listePoint2 - listPoint1) * Math.pow((1 - t), 2));
+			break;
+		default:
+			break;
+		}
 
-    public List<Coordonnee> getListePointTangente() {
-        return listePointTangente;
-    }
-    
-    public int getAngleInitial() {
-      return angleInitial;
-    }
-    
-    public int getVitesseInitial() {
-      return vitesseInitial;
-    }
-    
-    public double getVitesseY() {
-      return vitesseY;
-    }
-  
-  
+		return resultat;
+	}
+
+	public double getCoordX() {
+		return coordX;
+	}
+
+	public double getCoordY() {
+		return coordY;
+	}
+
+	public double getCoordXDeriv() {
+		return coordXDeriv;
+	}
+
+	public double getCoordYDeriv() {
+		return coordYDeriv;
+	}
+
+	public int getAngleInitial() {
+		return angleInitial;
+	}
+
+	public int getVitesseInitial() {
+		return vitesseInitial;
+	}
+
+	public double getVitesseY() {
+		return vitesseY;
+	}
+
 }
