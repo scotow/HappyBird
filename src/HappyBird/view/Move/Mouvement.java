fbe258ff70f6f00087dcad320c8127c24cbb3af7
housett <thomas.houset@gmail.com>
2015-11-22 18:09:00 +0100

    Positions x et y de l'oiseau selon sa courbe.

--- src/HappyBird/view/Move/Mouvement.java
+++ src/HappyBird/view/Move/Mouvement.java
@@ -1,4 +1,4 @@
-package HappyBird.view.ove;;
+package HappyBird.view.Move;
 
 /**
  * Cette classe concerne tous les mouvements (issus du Livrable 2), elle comprend les mouvements et orientation de l'oiseau
