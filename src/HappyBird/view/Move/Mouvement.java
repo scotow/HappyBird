7b6d48a4544bf7745cbb291f23dc85a81e9aaf42
housett <thomas.houset@gmail.com>
2015-11-22 17:53:34 +0100

    x

---
+++ src/HappyBird/view/Move/Mouvement.java
@@ -0,0 +1,11 @@
+package HappyBird.view.Move;
+
+/**
+ * Cette classe concerne tous les mouvements (issus du Livrable 2), elle comprend les mouvements et orientation de l'oiseau
+ * ainsi que ceux des obstacles.		- 
+ * @author housett
+ *
+ */
+public class Mouvement {
+
+}
