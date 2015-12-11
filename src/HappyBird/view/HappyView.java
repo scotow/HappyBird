2324c15be09130b1868881cbc63de943fe0ad6fa
Yann Garbé <yanngarbe@gmail.com>
2015-12-11 09:54:52 +0100

    Quelques modif pour preparer la courbe de thomas

--- src/HappyBird/view/HappyView.java
+++ src/HappyBird/view/HappyView.java
@@ -5,6 +5,7 @@ import HappyBird.Controller.CollisionControler;
 import HappyBird.Controller.PositionControler;
 import HappyBird.Object.Config.Constante;
 import HappyBird.model.PlateauModel;
+import HappyBird.view.Move.Mouvement;
 
 /**
  * 
@@ -16,6 +17,7 @@ public class HappyView {
   protected PlateauModel plateauModel;
   protected CollisionControler collisionControler;
   protected PositionControler controler;
+  protected Mouvement v;
   private ElementView elementView;
   private JFrame jFrame;
 
@@ -32,7 +34,7 @@ public class HappyView {
     this.plateauModel = plateauModel;
     this.collisionControler = collisionControler;
     this.controler = positionControler;
-    this.elementView = new ElementView(plateauModel, collisionControler, positionControler);
+    this.elementView = new ElementView(plateauModel, collisionControler, positionControler, v);
     jFrame = new JFrame(label);
     jFrame.add(elementView);
     jFrame.setSize(Constante.X_FRAME, Constante.Y_FRAME);
